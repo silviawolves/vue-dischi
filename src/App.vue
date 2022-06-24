49d9b202fc118d3d4b26f1aa1d8b30d624a3c550
silviawolves <silviacanepa.w@gmail.com>
2022-06-24 13:10:25 +0200

    base code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader />
+    <TheMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader,
+    TheMain
+}
 }
 </script>
 
@@ -21,8 +24,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheMain'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
