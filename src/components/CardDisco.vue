35bc160d1b7dc787fb6b32b818719c75905efa0b
silviawolves <silviacanepa.w@gmail.com>
2022-06-24 15:57:28 +0200

    push!

---
+++ src/components/CardDisco.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="my-card">
+        <img :src="discImg" :alt="`Album ${discTitle}`">
+        <div class="title">{{ discTitle }}</div>
+        <div class="author">{{ discAuthor }}</div>
+        <div class="year"> {{ discYear }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardDisco',
+    props: {
+        discImg: String,
+        discAuthor: String,
+        discTitle: String,
+        discYear: String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_variables.scss";
+
+.my-card {
+    background-color: $primary;
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+    text-align: center;
+
+    img {
+        aspect-ratio: 1/1;
+        width: 100%;
+        display: block;
+    }
+
+    .title {
+        padding: 1rem 0;
+        text-transform: uppercase;
+        font-size: 1.5rem;
+        color: white;
+        font-weight: bold;
+        line-height: 100%;
+    }
+
+    .author, .year {
+        color: gray;
+    }
+}
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,15 +1,40 @@
 <template>
     <div class="main">
-        <div class="container">
+        <div class="container py-5">
 
-
+            <div class="row row-cols-5 gx-5 gy-3">
+                <div class="col" v-for="(dischi, i) in discList" :key="i">
+                    <CardDisco :disc-img="dischi.poster" :disc-title="dischi.title" :disc-author="dischi.author" :disc-year="dischi.year"></CardDisco>
+                </div>
+            </div>
+            
         </div>
     </div>
 </template>
 
 <script>
+import CardDisco from './CardDisco.vue'
+import axios from 'axios'
+
 export default {
-    name: 'TheMain'
+    name: "TheMain",
+    components: { CardDisco },
+    data() {
+        return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            discList: [],
+        }
+    },
+    methods: {
+        fetchDiscList() {
+            axios.get(this.apiUrl).then((risultato) => {
+                this.discList = risultato.data.response
+            })
+        }
+    },
+    mounted() {
+        this.fetchDiscList()
+    }
 }
 </script>
 
